76a7d926fbe068cf08b13bcb6b4fdbc223164919
MorPhe23 <100770652+MorPhe23@users.noreply.github.com>
2023-09-10 21:07:50 +0800

    Added player input

--- app.js
+++ app.js
@@ -1,58 +1,95 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const currentTeam = document.getElementById('currentTeam');
-    const substitutes = document.getElementById('substitutes');
-    const substituteBtn = document.getElementById('substituteBtn');
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Player Substitution</title>
+</head>
+<body>
+    <h1>Player Substitution</h1>
+    <p>Select a player from the current team and a substitute to make a substitution.</p>
+    <div>
+        <h2>Current Team</h2>
+        <ul id="currentTeam"></ul>
+    </div>
+    <div>
+        <h2>Substitutes</h2>
+        <ul id="substitutes"></ul>
+    </div>
+    <button id="substituteBtn">Substitute</button>
 
-    // Sample data for current team and substitutes
-    const currentTeamPlayers = ['Player 1', 'Player 2', 'Player 3'];
-    const substitutePlayers = ['Substitute 1', 'Substitute 2', 'Substitute 3'];
+    <script>
+        document.addEventListener('DOMContentLoaded', function () {
+            const currentTeam = document.getElementById('currentTeam');
+            const substitutes = document.getElementById('substitutes');
+            const substituteBtn = document.getElementById('substituteBtn');
 
-    // Function to populate player lists
-    function populatePlayerLists() {
-        currentTeam.innerHTML = currentTeamPlayers.map(player => `<li>${player}</li>`).join('');
-        substitutes.innerHTML = substitutePlayers.map(player => `<li>${player}</li>`).join('');
-    }
+            // Initialize empty arrays for current team and substitutes
+            const currentTeamPlayers = [];
+            const substitutePlayers = [];
 
-    populatePlayerLists();
-
-    // Function to handle player substitution
-    function substitutePlayer() {
-        const selectedSubstitute = substitutes.querySelector('li.active');
-        if (selectedSubstitute) {
-            const playerToSubstitute = currentTeam.querySelector('li.active');
-            if (playerToSubstitute) {
-                const substituteName = selectedSubstitute.textContent;
-                const playerName = playerToSubstitute.textContent;
-
-                // Perform the substitution logic here
-                console.log(`Substituting ${substituteName} for ${playerName}`);
-
-                // Update the player lists and remove the 'active' class
-                currentTeamPlayers[currentTeamPlayers.indexOf(playerName)] = substituteName;
-                substitutePlayers[substitutePlayers.indexOf(substituteName)] = playerName;
-                selectedSubstitute.classList.remove('active');
-                playerToSubstitute.classList.remove('active');
-
-                // Repopulate the player lists
-                populatePlayerLists();
+            // Prompt for player input for the current team
+            for (let i = 1; i <= 8; i++) {
+                const playerName = prompt(`Enter the name of player ${i}`);
+                currentTeamPlayers.push(playerName);
             }
-        }
-    }
 
-    substituteBtn.addEventListener('click', substitutePlayer);
+            // Prompt for player input for the substitutes
+            for (let i = 1; i <= 8; i++) {
+                const playerName = prompt(`Enter the name of substitute ${i}`);
+                substitutePlayers.push(playerName);
+            }
 
-    // Add click event listeners to player items for selection
-    currentTeam.querySelectorAll('li').forEach(player => {
-        player.addEventListener('click', () => {
-            currentTeam.querySelectorAll('li').forEach(p => p.classList.remove('active'));
-            player.classList.add('active');
+            // Function to populate player lists
+            function populatePlayerLists() {
+                currentTeam.innerHTML = currentTeamPlayers.map(player => `<li>${player}</li>`).join('');
+                substitutes.innerHTML = substitutePlayers.map(player => `<li>${player}</li>`).join('');
+            }
+
+            populatePlayerLists();
+
+            // Function to handle player substitution
+            function substitutePlayer() {
+                const selectedSubstitute = substitutes.querySelector('li.active');
+                if (selectedSubstitute) {
+                    const playerToSubstitute = currentTeam.querySelector('li.active');
+                    if (playerToSubstitute) {
+                        const substituteName = selectedSubstitute.textContent;
+                        const playerName = playerToSubstitute.textContent;
+
+                        // Perform the substitution logic here
+                        console.log(`Substituting ${substituteName} for ${playerName}`);
+
+                        // Update the player lists and remove the 'active' class
+                        currentTeamPlayers[currentTeamPlayers.indexOf(playerName)] = substituteName;
+                        substitutePlayers[substitutePlayers.indexOf(substituteName)] = playerName;
+                        selectedSubstitute.classList.remove('active');
+                        playerToSubstitute.classList.remove('active');
+
+                        // Repopulate the player lists
+                        populatePlayerLists();
+                    }
+                }
+            }
+
+            substituteBtn.addEventListener('click', substitutePlayer);
+
+            // Add click event listeners to player items for selection
+            currentTeam.querySelectorAll('li').forEach(player => {
+                player.addEventListener('click', () => {
+                    currentTeam.querySelectorAll('li').forEach(p => p.classList.remove('active'));
+                    player.classList.add('active');
+                });
+            });
+
+            substitutes.querySelectorAll('li').forEach(player => {
+                player.addEventListener('click', () => {
+                    substitutes.querySelectorAll('li').forEach(p => p.classList.remove('active'));
+                    player.classList.add('active');
+                });
+            });
         });
-    });
+    </script>
+</body>
+</html>
 
-    substitutes.querySelectorAll('li').forEach(player => {
-        player.addEventListener('click', () => {
-            substitutes.querySelectorAll('li').forEach(p => p.classList.remove('active'));
-            player.classList.add('active');
-        });
-    });
-});
